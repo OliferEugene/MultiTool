4c7a41745324df7efcc51df7d18799098b1bfeb5
OliferEugene <oliferenko.eugene@outlook.com>
2018-10-20 20:32:51 +0300

    Edited EditProfile window. Bug fixes

--- src/company/controllers/EditProfileController.java
+++ src/company/controllers/EditProfileController.java
@@ -15,9 +15,13 @@ import java.util.ResourceBundle;
 
 import company.controllers.samples.DatabaseHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class EditProfileController {
 
